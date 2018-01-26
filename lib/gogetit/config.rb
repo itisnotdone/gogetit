01c96b2d63430f5b78c0a2d283df1d863042e99f
Don Draper <Donoldfashioned@gmail.com>
2018-01-26 07:12:57 +0000

    little changes on config's logging

--- lib/gogetit/config.rb
+++ lib/gogetit/config.rb
@@ -14,12 +14,12 @@ module Gogetit
     # TODO: It might be needed to disable one of logging devices according to satiation.
     STDOUT.sync = true
     log_to_stdout = Logger.new(STDOUT)
+    # Instantiate main objects
     @logger = Gogetit::MultiLogger.new(:loggers => log_to_stdout)
-    logger.info('Instantiate main objects..')
 
     @config = {}
 
-    logger.info('Defining home directory..')
+    # Define home directory
     user_gogetit_home = Dir.home + '/.gogetit'
     config[:user_gogetit_home] = user_gogetit_home
     if not File.directory?(user_gogetit_home)
@@ -28,10 +28,10 @@ module Gogetit
     end
 
     # TODO: It can be used to provide different behavior according to consumer.
-    logger.info('Defining default consumer..')
+    # Define default consumer
     config[:consumer] = 'gogetit_cli'
 
-    logger.info('Defining log directory..')
+    # Define log directory
     log_dir = user_gogetit_home + '/log'
     config[:log_dir] = log_dir
     if not File.directory?(log_dir)
@@ -39,20 +39,20 @@ module Gogetit
       FileUtils.mkdir(log_dir)
     end
 
-    logger.info('Define file log devices..')
+    # Define file log devices
     log_file = File.open(log_dir + '/debug.log', 'a')
     log_file.sync = true
     log_to_file = Logger.new(log_file)
     logger.add_logger(log_to_file)
 
-    logger.info('Defining logger..')
+    # Define logger
     # logger.datetime_format = "%Y-%m-%d %H:%M:%S"
     logger.progname = 'GoGetIt'
 
     lib_dir = Gem::Specification.find_by_name('gogetit').gem_dir + '/lib'
 
     config[:lib_dir] = lib_dir
-    logger.info('Loading GoGetIt default configuration..')
+    # Load GoGetIt default configuration
     conf_file = user_gogetit_home + '/gogetit.yml'
     if not File.exists?(conf_file)
       src = File.new(lib_dir + '/sample_conf/gogetit.yml')
